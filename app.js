7f647471d3a3d1628ef46a7fe8ee6728b7989749
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2024-03-05 04:38:25 +0100

    colors packages

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import {franc, francAll} from 'franc'
 import langs from 'langs'
+import colors from 'colors'
 
 const input = process.argv[2]
 const shortLang = franc(input)
 
 
 if(shortLang === 'und'){
-    console.log("try another sample text")
+    console.log("try another sample text".red)
 }else{
     const language = langs.where('3', shortLang)
-    console.log(`our best guess is ${language.name}`)
+    console.log(`our best guess is ${language.name.green}`)
 }
 
 
